7fd899d2fcbd89a425bfea5770ede212c3cbe7a2
nadezhdaaj <nad875q@gmail.com>
2026-06-16 09:17:20 +0500

    feat: remove trash

--- mart-backend copy/prisma/migrations/20260515055130_add_points_and_title/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "points" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "title" TEXT NOT NULL DEFAULT 'Новичок';
--- mart-backend copy/prisma/migrations/20260515092851_add_quiz_points/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "quiz1Points" INTEGER NOT NULL DEFAULT 0;
